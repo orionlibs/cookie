fb739f83d2e42d8cca13a0e5068347180cd49821
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-13 15:09:01 +0100

    implemented

---
+++ src/main/java/io/github/orionlibs/cookie/CookieDefaults.java
@@ -0,0 +1,9 @@
+package io.github.orionlibs.cookie;
+
+class CookieDefaults
+{
+    public static final String path = "/";
+    public static final String sameSite = "None";
+    public static final boolean HTTPOnly = true;
+    public static final boolean secure = true;
+}
